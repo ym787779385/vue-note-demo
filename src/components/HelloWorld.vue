4a5565c072572a1b3110338c6158771a4070ad3b
LAPTOP-4PS3DO9O\Y <manito13579@163.com>
2020-10-16 15:07:02 +0800

    初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,112 +1,7 @@
 <template>
-  <div class="hello">
+  <div id="menu">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha"
-          target="_blank"
-          rel="noopener"
-          >unit-mocha</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <!-- <div id="charts" style="width: 800px; height: 500px"></div> -->
   </div>
 </template>
 
@@ -115,6 +10,107 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  mounted() {
+    // this.initEcharts();
+  },
+  methods: {
+    initEcharts() {
+      let myChart = this.$echarts.init(
+        document.getElementById("charts"),
+        "dark"
+      );
+      // 绘制图表
+      myChart.setOption({
+        title: { text: "在Vue中使用echarts" },
+        tooltip: {},
+        // backgroundColor: '#2c343c',
+        // visualMap: {
+        //   show: false,
+        //   min: 80,
+        //   max: 600,
+        //   inRange: {
+        //     colorLightness: [0.1, 0]
+        //   }
+        // },
+        grid: {
+          left: 30,
+          right: 30,
+          height: '80%',
+          width: '80%',
+          bottom: 80
+        },
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        dataZoom: [
+          {
+            type: 'slider',
+            start: 10,
+            end: 60
+          }
+        ],
+        series: [
+          { type: "line", name: "数量", data: [35, 40, 50, 80] },
+          {
+            type: "bar",
+            name: "数量",
+            data: [15, 30, 20, 30],
+            color: [
+              "#ea7e53",
+              "#eedd78",
+              "#73a373",
+              "#73b9bc",
+              "#7289ab",
+              "#91ca8c",
+              "#f49f42"
+            ]
+          },
+          {
+            type: "pie",
+            radius: "25%",
+            center: [200, 140],
+            roseType: "angle", // 通过半径表示数据大小
+            label: {
+              normal: {
+                textStyle: { color: `rgba(255, 255, 255, 0.8)`}
+              }
+            },
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: "rgba(156, 178, 12, 0.9)"
+                }
+              }
+            },
+            itemStyle: {
+              // 鼠标的hover高亮样式
+              emphasis: {
+                shadowBlur: 200,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              },
+              normal: {
+                // color: "#c23531",
+                // 阴影大小
+                shadowBlur: 200,
+                // 阴影水平方向上的偏移
+                shadowOffsetX: 0,
+                // 阴影垂直方向上的偏移
+                shadowOffsetY: 0,
+                // 阴影颜色
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            },
+            data: [
+              { name: "xxx", value: 22 },
+              { name: "yy", value: 45 },
+              { name: "zz", value: 35 }
+            ]
+          }
+        ]
+      });
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import echarts from "echarts";
+import "./style/index.less";
 
 Vue.config.productionTip = false;
-
+Vue.prototype.$echarts = echarts;
 new Vue({
   router,
   store,
